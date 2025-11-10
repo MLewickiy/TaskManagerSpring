af8eac9e1f8cf51ed9481a55a34d139f350c6d48
lewicki <mlewickiy@yandex.ru>
2025-11-10 15:45:53 +0300

    Начало проекта

---
+++ src/main/java/ru/lewicki/taskmanagerspring/TaskManagerSpringApplication.java
@@ -0,0 +1,13 @@
+package ru.lewicki.taskmanagerspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagerSpringApplication.class, args);
+    }
+
+}
